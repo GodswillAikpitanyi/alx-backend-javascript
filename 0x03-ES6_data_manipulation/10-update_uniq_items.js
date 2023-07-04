fa52f0eeef95a98beee422066a2ab35b129e9cf0
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-04 22:42:42 +0000

    correction to task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,16 +1,11 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-
-  const updatedMap = new Map();
-  for (const [item, quantity] of map) {
-    if (quantity === 1) {
-      updatedMap.set(item, 100);
-    } else {
-      updatedMap.set(item, quantity);
+  if (map instanceof Map) {
+    for (const [key, value] of map.entries()) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
     }
+    return map;
   }
-
-  return updatedMap;
-}
+  throw new Error('Cannot process');
+};
