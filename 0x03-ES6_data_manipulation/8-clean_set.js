ade930c0633eb5455ffa12a9dc5a55cc1bd6278b
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-04 21:14:56 +0000

    mandatory tasks 6 - 10

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  const valuesArray = Array.from(set);
+  const filteredArray = valuesArray.filter(value => value.startsWith(startString));
+  const cleanedString = filteredArray.map(value => value.slice(startString.length)).join('-');
+  return cleanedString;
+}
+
