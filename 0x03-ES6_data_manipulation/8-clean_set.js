20c9682fe5b293c414e08f07a8492f7c28de26b8
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-04 23:01:37 +0000

    correction to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
-  const valuesArray = Array.from(set);
-  const filteredArray = valuesArray.filter((value) => value.startsWith(startString));
-  const cleanedString = filteredArray.map((value) => value.slice(startString.length)).join('-');
-  return cleanedString;
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+    .join('-');
 }
