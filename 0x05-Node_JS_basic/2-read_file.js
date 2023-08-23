a8e01fd22b4e65dc19a94db1eb0962e63b1ae54f
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-23 20:53:01 +0000

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n');
+
+    if (lines.length <= 1) {
+      throw new Error('No valid students found in the database');
+    }
+
+    const allFields = {};
+    for (let i = 1; i < lines.length; i += 1) {
+      const [, , , field] = lines[i].split(',');
+      if (field) {
+        if (allFields[field]) {
+          allFields[field].count = +1;
+          allFields[field].list.push(lines[i].split(',')[0]);
+        } else {
+          allFields[field] = { count: 1, list: [lines[i].split(',')[0]] };
+        }
+      }
+    }
+
+    console.log(`Number of students: ${lines.length - 1}`);
+    for (const field in allFields) {
+      if (Object.prototype.hasOwnProperty.call(allFields, field)) {
+        console.log(`Number of students in ${field}: ${allFields[field].count}. List: ${allFields[field].list.join(', ')}`);
+      }
+    }
+  } catch (error) {
+    console.error(`Cannot load the database: ${error.message}`);
+  }
+}
+
+module.exports = countStudents;
